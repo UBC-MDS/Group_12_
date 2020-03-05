0fa07a217fd55c572609e2d89b154df4d26e1722
margaret8521 <margaretzhao@yahoo.com>
2020-03-05 13:30:45 -0800

    fixed minor errors in test_compress and pass the test

--- tests/test_compress.py
+++ tests/test_compress.py
@@ -1,16 +1,17 @@
-from group_12.group_12 import shrink, sharpen, compress
+from group_12.compress import compress
 from numpy import random
 
-img = random.random((50,50,3))
+#img = random.random((50,50,3))
 
 def test_compress():
     '''
     This function test the output image of the compress function,
     checking the shape and type is correct.
     '''
-  
+    img = random.random((50,50,3))
     img2 = compress(img, 3)
     
-    assert img2.shape[0] == img.shape[0]
+    # assert img2.shape[0] == img.shape[0]
     assert img2.shape[1] == img.shape[1]
     assert len(img2.shape) == 3
+    assert img2.shape[0] == img.shape[0]
