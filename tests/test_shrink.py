7873ed3a05cfcd927630c0af152c59ed26648c04
Rita Ni <niruidan@gmail.com>
2020-03-05 19:40:36 -0800

    added author and removed duplicate tests

--- tests/test_shrink.py
+++ tests/test_shrink.py
@@ -8,38 +8,21 @@ image = random.random((50,50,3))
 image_wrong_type = [1,2,3]
 image_wrong_dim = random.random((50,50))
 
-class test_shrink():
+def test_crop_output():
+    '''
+    This function test the output image of the shrink function,
+    checking the shape and type is correct.
+    '''
+    assert shrink(image, 20, 20).shape[0:2] == (20, 20)
 
-    def test_crop_output():
-        '''
-        This function test the output image of the shrink function,
-        checking the shape and type is correct.
-        '''
-        assert shrink(image, 20, 20).shape[0:2] == (20, 20)
+    assert shrink(image, 15, 15).shape[0:2] == (15, 15)
 
-        assert shrink(image, 15, 15).shape[0:2] == (15, 15)
+    assert shrink(image, 20, 20).shape[0:2] == (6, 6)
 
-        assert shrink(image, 20, 20).shape[0:2] == (6, 6)
+    assert shrink(image, 20, 20).shape[3] == 3
 
-        assert shrink(image, 20, 20).shape[3] == 3
+    assert len(shrink(image, 20, 20).shape) == 3
 
-        assert len(shrink(image, 20, 20).shape) == 3
-
-    def test_shrink_error():
-        '''
-        This function test the error handling when invalid inputs
-        are entered
-        '''
-        with pytest.raises(ValueError):
-            shrink(image, 5.1, 5)
-        with pytest.raises(ValueError):
-            shrink(image, -1, 1)
-        with pytest.raises(ValueError):
-            shrink(image, 100, 100)
-        with pytest.raises(TypeError):
-            shrink(image_wrong_type, 10, 10)
-        with pytest.raises(TypeError):
-            shrink(image_wrong_dim, 10, 10)
 
 def test_shrink_error():
         '''
