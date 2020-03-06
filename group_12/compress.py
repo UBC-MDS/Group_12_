02f25bd61b66229421495b3d188a4a6d2ace7f37
margaret8521 <margaretzhao@yahoo.com>
2020-03-05 17:23:32 -0800

    add comments in function and add more tests

--- group_12/compress.py
+++ group_12/compress.py
@@ -20,26 +20,28 @@ def compress(img, b):
     
     """
 
-
     # inpute tests
     if type(b) != int:
         raise TypeError("b should be an integer!")
     if b <= 0:
         raise ValueError("b should be positive!")
-    if img.ndim != 3:
-        raise TypeError("The dimension of the input image must be 3D")
+    if (img.ndim != 3) or (type(img) != np.ndarray):
+        raise TypeError("The dimension of the input image must be 3D array")
         
         
-    # the function
+    # the function 
     H, W, _ = img.shape
+    # use KMeans for compression
     model = KMeans(n_clusters=2**b)
-    
+    # reshape the image
     image_array = np.reshape(img, (H * W, 3))
-    
+    # fit the model using KMeans
     model.fit(image_array)
+    # reshape labels
     quantized_img = model.labels_.reshape(H, W)
+    # find the cluster centers
     colours = model.cluster_centers_.astype('uint8')
-    
+    # dequantized where the original color is replaced with the nearest prototype colour
     img_quan = colours[quantized_img]
     
     return img_quan
