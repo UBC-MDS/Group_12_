13c4c8a7a9c4b36cbc02098b327a0c047d03b673
Frank Lu <franklu@live.ca>
2020-03-26 00:39:55 -0700

    Address comment: normalize brightness of input array

--- imgtoolpy/sharpen.py
+++ imgtoolpy/sharpen.py
@@ -17,33 +17,38 @@ def sharpen(img):
     Parameters
     ----------
     img : a (H,W,3) numpy array
-      the image to be processed
+        the image to be processed
 
     Returns
     --------
-    img : a (H,W) numpy array, returns the sharpened and monotoned image
+    (img2, img2_sharpened) : two (H,W) numpy arrays; returns the original and sharpened images, both monotoned
+        
+    Examples
+    --------
+    >>> sharpen(image)
 
     """
-    if (type(img) != np.ndarray) or (len(img.shape) != 3):
-        raise TypeError('Invalid Type: input type for image must be 3D array')
+    if (type(img) != np.ndarray) or (len(img.shape) != 3) or (img.shape[2] != 3):
+        raise TypeError('Invalid Type: input type for image must be 3D H*W*3 array')
     # make sure the input image is at least 10x of the filter
     if (img.shape[0] < 50 or img.shape[1] < 50):
         raise ValueError(
             'Input image should have height and width greater than 50 x 50')
     if (np.min(img) < 0 or np.max(img) > 1):
-        raise ValueError('Input image should have values between 0 and 1')
+        img = (img - np.min(img)) / (np.max(img) - np.min(img))
+        print("Image brightness normalized")
 
     img2 = rgb2gray(img)
 
     # Create a 2-D array with 1 in the center and negative
     # otherwise to highlight the edge
-    flt = -np.ones((3, 3))/22
+    n = 3
+    N = n * 2 + 1
+    flt = -np.ones((N, N)) / (16 * n**2)
     flt[1, 1] = 1
 
     I_filt = convolve2d(img2, flt, boundary='symm', mode='same')
-    # set negative values to 0, for visualization purposes
-    I_filt = np.maximum(0, I_filt)
-    # set values greater than 1 to 1, for visualization purposes
-    I_filt = np.minimum(1, I_filt)
+    # normalize the brightness of the sharpened image
+    img2_sharpened = (I_filt - np.min(I_filt)) / (np.max(I_filt) - np.min(I_filt))
 
-    return (I_filt - np.min(I_filt)) / (np.max(I_filt) - np.min(I_filt))
+    return img2, img2_sharpened
--- tests/test_sharpen.py
+++ tests/test_sharpen.py
@@ -3,26 +3,30 @@ import numpy as np
 from numpy import random
 import pytest
 
-img = read_image("img/milad_cropped.png")
-img2 = sharpen(img)
+img = read_image("img/free-wallpaper.jpg")
+img_orig, img_sharpened = sharpen(img)
 img3 = random.random((20, 20, 3))
 img4 = random.random((60, 60))
-img5 = np.array([-0.2] * (60 * 60 * 3)).reshape((60, 60, 3))
+img5 = random.random((60, 60, 4))
 
 # class test_sharpen():
 
 
 def test_read_image():
     assert len(img.shape) == 3
-    assert img.shape[0] == 372
-    assert img.shape[1] == 372
-    assert img.shape[2] == 4
+    assert img.shape[0] == 1066
+    assert img.shape[1] == 1599
+    assert img.shape[2] == 3
 
 
 def test_sharpen():
-    assert img2.shape[0] == 372
-    assert img2.shape[1] == 372
-    assert len(img2.shape) == 2
+    assert img_orig.shape[0] == 1066
+    assert img_orig.shape[1] == 1599
+    assert len(img_orig.shape) == 2
+    
+    assert img_sharpened.shape[0] == 1066
+    assert img_sharpened.shape[1] == 1599
+    assert len(img_sharpened.shape) == 2
 
 
 def test_sharpen_error():
@@ -30,5 +34,5 @@ def test_sharpen_error():
         sharpen(img3)
     with pytest.raises(TypeError):
         sharpen(img4)
-    with pytest.raises(ValueError):
+    with pytest.raises(TypeError):
         sharpen(img5)
